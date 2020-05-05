74218baa2ed8a957950c59afdda90adaae176817
lihonghao <1345455432@qq.com>
2020-05-05 10:05:44 +0800

    init

---
+++ src/test/java/com/lhh/scheduletask/ScheduletaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhh.scheduletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduletaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
